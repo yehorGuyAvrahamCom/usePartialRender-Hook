57c84d09921aa62e915c7fe41df4ef6f35158b01
yehorGuyAvrahamCom <yehor@guy-avraham.com>
2021-10-04 10:27:30 +0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { Box, Flex } from '@chakra-ui/react';
 import { usePartialRender } from './usePartialRender';
 
-const list = Array(30)
+const list = Array(10000)
   .fill(0)
   .map((_, index) => index + 1);
 
-const App = () => {
+const Grid = ({ left, top }: any) => {
   const ref = useRef(null);
   const elementHeight = 62;
   const elementSize = 164;
@@ -14,46 +14,107 @@ const App = () => {
   const vertical = usePartialRender({
     ref,
     elementSize: elementHeight,
+    verticalRef: left,
   });
 
   const horizontal = usePartialRender({
     ref,
     elementSize,
     direction: 'horizontal',
+    horizontalRef: top,
   });
 
   const horizontalList = list.slice(horizontal.from, horizontal.to);
   const verticalList = list.slice(vertical.from, vertical.to);
 
+  // console.log(vertical, horizontal);
+
   return (
-    <Flex align="center" justify="center" w="100vw" h="100vh">
-      <Box
-        className="hide-scrollbar"
-        ref={ref}
-        h="600px"
-        w="600px"
-        overflow="auto"
-        bg="gray.50"
-      >
-        {verticalList.map((row, rowIndex) => (
-          <Flex key={rowIndex}>
-            {horizontalList.map((column, columnIndex) => (
-              <Flex
-                key={columnIndex}
-                minW={`${elementSize}px`}
-                h={`${elementHeight}px`}
+    <Box
+      className="hide-scrollbar"
+      ref={ref}
+      h="full"
+      w="full"
+      overflow="auto"
+      bg="gray.50"
+    >
+      {verticalList.map((row, rowIndex) => (
+        <Flex key={rowIndex}>
+          {horizontalList.map((column, columnIndex) => (
+            <Flex
+              key={columnIndex}
+              minW={`${elementSize}px`}
+              h={`${elementHeight}px`}
+              border="1px"
+              borderColor="gray.200"
+              boxSizing="border-box"
+              align="center"
+              justify="center"
+            >
+              {`${row} - ${column}`}
+            </Flex>
+          ))}
+        </Flex>
+      ))}
+    </Box>
+  );
+};
+
+const App = () => {
+  const refLeft = useRef(null);
+  const refHeader = useRef(null);
+  const elementHeight = 62;
+  const elementSize = 164;
+
+  // const vertical = usePartialRender({
+  //   ref: refLeft,
+  //   elementSize: elementHeight,
+  // });
+  // const horizontal = usePartialRender({
+  //   ref: refLeft,
+  //   elementSize: elementHeight,
+  // });
+
+  // const verticalList = list.slice(vertical.from, vertical.to);
+
+  return (
+    <Box id="PARENT" h="100vh" padding="10">
+      <Box id="calendar" h="full">
+        <Flex id="header" ml="55px" overflowX="hidden" ref={refHeader}>
+          {list.map((item) => (
+            <Box
+              key={item}
+              bg="gray.100"
+              height="40px"
+              minW="164px"
+              boxSizing="border-box"
+              border="1px"
+              borderColor="gray.300"
+            >
+              {item}
+            </Box>
+          ))}
+        </Flex>
+        <Flex id="body" h="90%">
+          <Box id="left" ref={refLeft} minW="55px" overflowY="hidden" h="full">
+            {list.map((item) => (
+              <Box
+                key={item}
+                bg="red.100"
+                height="62px"
+                minW="55px"
+                boxSizing="border-box"
                 border="1px"
-                borderColor="gray.200"
-                align="center"
-                justify="center"
+                borderColor="red.300"
               >
-                {`${row} - ${column}`}
-              </Flex>
+                {item}
+              </Box>
             ))}
-          </Flex>
-        ))}
+          </Box>
+          <Grid left={refLeft} top={refHeader} />
+        </Flex>
       </Box>
-    </Flex>
+    </Box>
   );
 };
 
--- src/usePartialRender.tsx
+++ src/usePartialRender.tsx
@@ -10,12 +10,16 @@ export interface IUsePartialRenderProps {
   ref?: MutableRefObject<null>;
   elementSize: number;
   direction?: 'vertical' | 'horizontal';
+  horizontalRef?: MutableRefObject<null>;
+  verticalRef?: MutableRefObject<null>;
 }
 
 export const usePartialRender = ({
   ref,
   elementSize,
   direction = 'vertical',
+  horizontalRef,
+  verticalRef,
 }: IUsePartialRenderProps) => {
   const [shift, setShift] = useState(0);
   const [containerSize, setContainerSize] = useState(0);
@@ -37,13 +41,13 @@ export const usePartialRender = ({
     () => elementSize / containerSize,
     [elementSize, containerSize]
   );
-  const displayItemsCount = useMemo(
+  const itemsCount = useMemo(
     () => Math.floor((containerSize / elementSize) * (1.4 + ratio)),
     [containerSize, elementSize, ratio]
   );
   const scrollSize = useMemo(
-    () => elementSize * displayItemsCount - containerSize,
-    [elementSize, displayItemsCount, containerSize]
+    () => elementSize * itemsCount - containerSize,
+    [elementSize, itemsCount, containerSize]
   );
   const scrollDownBrakePoint = useMemo(
     () => scrollSize * (1 - ratio * 0.4),
@@ -53,6 +57,25 @@ export const usePartialRender = ({
     () => scrollSize - scrollDownBrakePoint,
     [scrollSize, scrollDownBrakePoint]
   );
+
+  const updateVerticalContainer = useCallback(() => {
+    const container = ref?.current as Element | null;
+    const verticalContainer = verticalRef?.current as Element | null;
+
+    if (container && verticalContainer) {
+      verticalContainer.scrollTop = container.scrollTop + shift * elementSize;
+    }
+  }, [ref, verticalRef, elementSize, shift]);
+
+  const updateHorizontalContainer = useCallback(() => {
+    const container = ref?.current as Element | null;
+    const horizonalContainer = horizontalRef?.current as Element | null;
+
+    if (container && horizonalContainer) {
+      horizonalContainer.scrollLeft =
+        container.scrollLeft + shift * elementSize;
+    }
+  }, [ref, horizontalRef, elementSize, shift]);
 
   const handleScroll = useCallback(() => {
     const container = ref?.current as Element | null;
@@ -64,20 +87,27 @@ export const usePartialRender = ({
       if (scrollPosition < scrollUpBrakePoint && shift > 0) {
         if (direction === 'vertical') {
           container.scrollTop += elementSize;
+          updateVerticalContainer();
         } else {
           container.scrollLeft += elementSize;
+          updateHorizontalContainer();
         }
 
         setShift((prev) => prev - 1);
       } else if (scrollPosition > scrollDownBrakePoint) {
         if (direction === 'vertical') {
           container.scrollTop -= elementSize;
+          updateVerticalContainer();
         } else {
           container.scrollLeft -= elementSize;
+          updateHorizontalContainer();
         }
 
         setShift((prev) => prev + 1);
       }
+
+      updateVerticalContainer();
+      updateHorizontalContainer();
     }
   }, [
     shift,
@@ -86,6 +116,8 @@ export const usePartialRender = ({
     elementSize,
     direction,
     ref,
+    updateHorizontalContainer,
+    updateVerticalContainer,
   ]);
 
   useEffect(() => {
@@ -96,5 +128,10 @@ export const usePartialRender = ({
     return () => container?.removeEventListener('scroll', handleScroll);
   }, [handleScroll, ref, direction]);
 
-  return { from: shift, to: shift + displayItemsCount };
+  return {
+    from: shift,
+    to: shift + itemsCount,
+    size: elementSize * itemsCount,
+    itemsCount,
+  };
 };
